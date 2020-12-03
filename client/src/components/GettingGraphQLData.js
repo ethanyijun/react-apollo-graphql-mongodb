77e1eae13889a0a8d1fe755d19562416e4ce3702
yijungai <ethan.yijun@gmail.com>
2020-12-03 20:52:44 +1100

    show list based on input value

--- client/src/components/GettingGraphQLData.js
+++ client/src/components/GettingGraphQLData.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLazyQuery } from "@apollo/client";
 import './GettingGraphQLData.css';
 import {
     useQuery,
@@ -6,27 +7,53 @@ import {
   } from "@apollo/client";
 
 function GettingGraphQLData() {
-    const { loading, error, data } = useQuery(gql`
-        {
+    const [input, setInput] = useState("");
+
+    const GET_PEOPLE = input === ""? gql`
+        query People {
             people {
                 id
                 firstName
                 lastName
             }
         }
-    `);
+        `:
+        gql`
+        query People($firstName: String!) {
+            peopleByName(firstName: $firstName) {
+                id
+                firstName
+                lastName
+            }
+        }
+        `;
+
+    const [doRequest, { loading, data }] = useLazyQuery(GET_PEOPLE);
+    const handleNameChange = e => {
+        setInput(e.target.value);
+        console.log(input);
+    };
   
     if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+
   
     return (
         <>
             <div className="list-type5">
+            <input type="text" value={input} onChange={handleNameChange} />
+            <button onClick={() => doRequest({ variables: { firstName: input } })}>
+                Find
+            </button>
                 <h3>Graph List</h3><br/>
                 <ol>
-                {data?.people?.map(p => (
+                {input === ""?
+                data?.people?.map(p => (
                     <li key={p.id}><a>{p.firstName} {p.lastName}</a></li>
-                ))}
+                )): data?.peopleByName?.map(p => (
+                    <li key={p.id}><a>{p.firstName} {p.lastName}</a></li>
+                ))
+                }
+                {}
                 </ol>
             </div>
         </>
